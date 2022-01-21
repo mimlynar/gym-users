86cd9d7fda7b8be0b1ec6488779707e75b8ce179
mmlynarczyk <misiek0408>
2022-01-21 15:24:56 +0100

    initial commit

---
+++ src/main/java/com/mmlynarczyk/gymusers/GymUsersApplication.java
@@ -0,0 +1,13 @@
+package com.mmlynarczyk.gymusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmlynarczyk/gymusers/GymUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmlynarczyk.gymusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
